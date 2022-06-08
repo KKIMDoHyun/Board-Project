7b9a2e0042fe0741963626dde33a86f0a7977779
dohyun_kim <dohyun_kim4@tmax.co.kr>
2022-06-08 09:54:35 +0900

    [config] 환경 세팅 (ReactNative, NestJS)

---
+++ login_frontend/App.tsx
@@ -0,0 +1,20 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React, {FC} from 'react';
+import {Text, View} from 'react-native';
+
+const App: FC = () => {
+  return (
+    <View>
+      <Text>DDD</Text>
+    </View>
+  );
+};
+
+export default App;
